e519b17cd91856ba61e2c877f36a2f933470bb92
dcts <thomas@liist.com>
2022-06-22 00:33:56 +0200

    :memo: add dev environment

--- init-dev-env.js
+++ init-dev-env.js
@@ -1,5 +1,12 @@
 // load packages
-const cheerio = require("cheerio");
+const axios = require("axios");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 // load scraper
 const LicenseScraper = require("./src/index.js");
+
+// GREET DEVELOPER
+console.log("\n\n");
+console.log(fs.readFileSync('init-dev-env-message.txt', 'utf8'));
+console.log("\n\n");
