1d247de0fa58e272fd6449fb74cee0f50a0ca6d9
dcts <thomas@liist.com>
2022-06-22 15:15:27 +0200

    :sparkle: add easy test function to dev environment/playground

--- init-dev-env.js
+++ init-dev-env.js
@@ -3,11 +3,26 @@ const axios = require("axios");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-// load helpers
-const { getDom } = require("./src/functions/helper.js");
 // load scraper
 const LicenseScraper = require("./src/index.js");
 
+// load helpers
+const { getDom } = require("./src/functions/helper.js");
+const test = async (type, name) => {
+  if (type === "ruby") {
+    console.log(`\n\n💎 RubyGems Testing: ${name}`);
+    const res = await LicenseScraper.rubyGem(name);
+    console.log(res);
+  } else if (type === "pod") {
+    console.log(`\n\n🐨 CocoaPods Testing: ${name}`);
+    const res = await LicenseScraper.cocoaPod(name);
+    console.log(res);
+  } else if (type === "npm") {
+    console.log(`\n\n💻️ npm Testing: ${name}`);
+    const res = await LicenseScraper.npmPackage(name);
+    console.log(res);
+  }
+}
 
 // GREET DEVELOPER
 console.log("\n\n");
